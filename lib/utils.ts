7cf66f3032273b9b46508c9d0a6f3fe17282a7c4
Kenil Shah <kenilshah93@gmail.com>
2026-08-21 14:32:11 +0530

    nit

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@
  * truncate("Hello World", 8) // "Hello..."
  * truncate("Hi", 5)          // "Hi"
  */
-export function truncate(str: string, maxLength: number, suffix = "..."): string {
+export function truncate(str: string, maxLength: number): string {
   if (str.length <= maxLength) return str;
-  return str.slice(0, maxLength) + suffix;
+  return str.slice(0, maxLength) + '...';
 }
